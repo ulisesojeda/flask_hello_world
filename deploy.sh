5ff7de79152b4f287f27bc393cf59dcba218bfab
Ulises Ojeda <ulises.odysseus22@gmail.com>
2020-09-15 18:48:15 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 sudo docker build -t flask_app .
-sudo docker kill `cat docker.cid`
-sudo rm docker.cid
-sudo docker run --name flask_app --cidfile docker.cid -p 80:5000 -d flask_app
+sudo docker rm flask_app -f | true
+sudo docker run --name flask_app -p 80:5000 -d flask_app
 echo $?
