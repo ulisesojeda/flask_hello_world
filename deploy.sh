02d9484be2b8cba176e10431443f617db674948a
Ulises Ojeda <ulises.odysseus22@gmail.com>
2020-09-15 14:29:54 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-sudo docker build -t static_website .
+sudo docker build -t flask_app .
 sudo docker kill `cat docker.cid`
 sudo rm docker.cid
-sudo docker run --name flask_app --cidfile docker.cid -p 80:5000 -d static_website
+sudo docker run --name flask_app --cidfile docker.cid -p 80:5000 -d flask_app
 echo $?
