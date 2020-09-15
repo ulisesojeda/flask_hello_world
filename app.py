0e7f09dabf42aec877997dc25054bd2f4f3bdc92
Ulises Ojeda <ulises.odysseus22@gmail.com>
2020-09-15 18:53:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ server = Flask(__name__)
 
 @server.route('/')
 def hello_world():
-    return 'hello world! v2'
+    return 'hello world! v3'
 
 if __name__ == '__main__':
     server.run(host='0.0.0.0')
