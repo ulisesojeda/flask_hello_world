ba408f4017bf6a7d691f9726f1fc9eafe0684894
Ulises <ojeda.ulises@bcg.com>
2020-09-16 11:42:37 +0200

    amends

--- app.py
+++ app.py
@@ -3,13 +3,8 @@ from flask import Flask, render_template
 server = Flask(__name__)
 
 @server.route('/')
-<<<<<<< HEAD
 def index():
     return render_template('index.html')
-=======
-def hello_world():
-    return 'hello world! v4'
->>>>>>> 0845e211d8d12aa073a2466bc327a9908874f97d
 
 if __name__ == '__main__':
     server.run(host='0.0.0.0')
--- test_f1.py
+++
@@ -1,2 +0,0 @@
-def test_always_passes():
-    assert 1 == 1
