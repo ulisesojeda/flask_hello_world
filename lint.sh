43cd42e6bf9654a944cc31db8d1a9b1083ae98d0
Ulises Ojeda <ulises.odysseus22@gmail.com>
2020-09-16 10:16:53 +0200

    Update lint.sh

--- lint.sh
+++ lint.sh
@@ -1,1 +1,1 @@
-find /usr/src/ -name "*.py" | grep -v env | while read file; do python -m flake8 --exit-zero $file; done
+docker exec flask_app /bin/sh -c "find /usr/src/ -name '*.py' | grep -v env | while read file; do python -m flake8 --exit-zero $file; done"
